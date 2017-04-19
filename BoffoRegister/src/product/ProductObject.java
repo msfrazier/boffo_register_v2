fa01ea408f550c4d3d174ab56658e74e6d0bbc90
John Kaiserlik <jrkaise2@uncg.edu>
2017-04-19 18:37:32 -0400

    removed white space

--- BoffoRegister/src/product/ProductObject.java
+++ BoffoRegister/src/product/ProductObject.java
@@ -32,90 +32,89 @@ public class ProductObject extends BoffoDbObject implements TicketElement{
        this.tableName = _tableName;
        this.description = _description;
     }
-    
+
     @Override
     public TicketElement clone(){
-        return TicketElement.class.cast(this);
+        return (TicketElement) new ProductObject();
     }
-    
-    
+
+
     public void setDescription(String _description) {
         this.description = _description;
     }
-    
-    
+
+
     @Override
     public String getDescription() {
         return this.description;
     }
-    
-    
+
+
     public void setName(String _name) {
         this.name = _name;
     }
-    
-    
+
+
     @Override
     public String getName() {
         return this.name;
     }
-    
-    
+
+
     public void setPrice(double _price) {
         this.price = _price;
     }
-    
-    
+
     @Override
     public double getPrice() {
         return this.price;
     }
-     
-    
+
+
     public ProductObject getProduct() {
         return new ProductObject(this.name, this.quantity, this.price, this.UPC, this.SKU, this.rat, this.uuid, this.tableName, this.description);
     }
-    
+
+ 
     public void setQuantity(int _quant) {
         this.quantity = _quant; 
     }
-    
-    
+
+
     public int getQuantity() {
         return this.quantity;
     }
-    
-    
+
+
     public void setRating(Rating _rat) {
         this.rat = _rat;
     }
-    
-    
+
+
     public Rating getRating() {
         return this.rat;
     }
-    
-    
+
+
     public void setUPC(int _upc) {
         this.UPC = _upc;
     }
-    
-    
+
+
     public int getUPC() {
         return this.UPC;
     }
-    
-    
+
+
     public void setSKU(String _sku) {
         this.SKU = _sku;
     }
-    
+
     @Override
     public String getSku() {
         return this.SKU;
     }
-    
-    
+
     @Override
     public String toString() {
         String str = "Name: " + this.getName() + "\n" + 
@@ -128,18 +127,18 @@ public class ProductObject extends BoffoDbObject implements TicketElement{
                      "Description: " + this.description + "\n";
         return str;
     }
-    
+
     //ProductObject.load() is a BoffoDbObject so still cast as ProductObject
     public static ProductObject loadBySKU(String _sku) {
         return (ProductObject)ProductObject.load("sku", _sku, "product");
     }
-    
-    
+
+
     public static ProductObject loadByUPC(String _upc) {
         return (ProductObject)ProductObject.load("upc", _upc, "product");
     }
-    
-    
+
+
     public static ProductObject loadByName(String _name) {
        return (ProductObject)ProductObject.load("name", _name, "product");
     }
@@ -148,18 +147,18 @@ public class ProductObject extends BoffoDbObject implements TicketElement{
     public static ProductObject loadByQuantity(String _quant) {
        return (ProductObject)ProductObject.load("quantity", _quant, "product");
     }
-    
-    
+
+
     public static ProductObject loadByRating(String _rat) {
         return (ProductObject)ProductObject.load("rating", _rat, "product");
     }
-    
-    
+
+
     public static ProductObject loadByPrice(String _price) {
         return (ProductObject)ProductObject.load("price", _price, "product");
     } 
-    
-    
+
+
     public static void main(String[] args) {
 
     }

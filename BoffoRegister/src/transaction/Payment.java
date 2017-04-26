26a4d0032d944dab16bafe88097a313eca854f8e
emabelyn <m_espino@uncg.edu>
2017-04-26 17:42:45 -0400

    Update Payment
    Update Payment

---
+++ BoffoRegister/src/transaction/Payment.java
@@ -0,0 +1,25 @@
+package transaction;
+
+public class Payment extends Transaction{
+    protected double amount;
+    
+    public Payment(){
+        this.amount = 0.00;
+    }
+    
+    public Payment(double _amount){
+        this.amount = _amount;
+    }
+    
+    public double getAmount(){
+        return this.amount;
+    }
+    
+    public void setAmount(double _amount){
+        this.amount = amount;
+    }
+    public String toString(){
+        return "Amount: " + amount;
+    }
+    
+}

b274be0f1ea9ea4f3b3193c4263ac939ac0aa92b
cdhansen96 <cdhanse2@uncg.edu>
2017-04-26 18:35:38 -0400

    Added Code to class

--- BoffoRegister/src/a/net/ChargeCreditCard.java
+++ BoffoRegister/src/a/net/ChargeCreditCard.java
@@ -5,8 +5,21 @@ import administration.Administration;
 class ChargeCreditCard extends Administration {
     final String transactionKey = getTransactionKey();
     final String SecretKey = getSecretKey();
-
-    public void ChargeCard(int cardNum, int expDate, double chargeAmt){
+    double amtCharge;
+    int cardNumber;
+    int expDate;
+  
     
+    
+    public String ChargeCard(int cardNum, int expDate, double chargeAmt){
+        this.cardNumber = cardNum;
+        this.expDate = expDate;
+        this.amtCharge = chargeAmt;
+        
+        return "ERR";
+    }
+    
+    public static ANetApiResponse run(String apiLoginId, String transactionKey, Double amount){
+        
     }
 }

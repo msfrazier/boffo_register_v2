d08e4524b9a5e94897dd6e3a606cf4d0f6a25b81
596854827 <596854827@qq.com>
2017-04-26 00:13:16 -0400

    Updata
    Updata method

--- BoffoRegister/src/transaction/Ticket.java
+++ BoffoRegister/src/transaction/Ticket.java
@@ -6,55 +6,60 @@ import java.util.ArrayList;
 import java.util.List;
 import product.ProductObject;
 
-public class Ticket extends Transaction {
-
+public class Ticket extends Transaction{
+    
     private List<ProductObject> products;
-
+    
     Ticket(List<ProductObject> products) {
-
+        
     }
-
+    
     public Ticket() {
-
+        
     }
-
-    public ProductObject addProductUPC(int UPC) {
-        // ProductObject product = (ProductObject) product.add(null, null);
-        // this.products.add(product);
-        // return product;
-        return null;
+    
+    public ProductObject addProductbyUPC(String UPC) {
+        ProductObject product = (ProductObject) ProductObject.loadByUpc(UPC);
+        this.products.add(product);
+        return product;
     }
-
-    public ProductObject addProductName(String name) {
-        return null;
+    
+    public ProductObject addProductbyName(String name) {
+        ProductObject product = (ProductObject) ProductObject.loadByName(name);
+        this.products.add(product);
+        return product;
     }
-
-    public ProductObject removeProductUPC(int UPC) {
-
-        return null;
+    
+    public void removeProductbyUPC(String UPC) {
+        ProductObject product = (ProductObject) ProductObject.loadByUpc(UPC);
+        this.products.remove(product);
     }
-
-    public ProductObject removeProductName(String name) {
-
-        return null;
+    
+    public void removeProductbyName(String name) {
+        ProductObject product = (ProductObject) ProductObject.loadByName(name);
+        this.products.remove(product);
     }
-
-    public double getTotalPrice() {
+    
+    public double getTotalPrice(String String_price) {
         double totalPrice = 0;
-//	for (int i=0; i< ProductObject.size(); i++) {
-//	    Object obj = ProductObject.get(i);
-//	    Product p = (ProductObject)obj;
-//	    totalPrice = totalPrice + p.getPrice();
-//	}
+        for (int i = 0; i < products.size(); i++) {
+            Object obj = ProductObject.loadByPrice(String_price);
+            ProductObject p = (ProductObject) obj;
+            totalPrice = totalPrice + p.getPrice();
+        }
         return totalPrice;
     }
-
+    
     public void fireEvent(BoffoEvent _event) {
     }
-
+    
     public void addListener(BoffoListenerInterface _event) {
     }
-
+    
     public void removeListener(BoffoListenerInterface _event) {
+    }
+    
+    public void messageReceived(BoffoEvent event) {
+
     }
 }

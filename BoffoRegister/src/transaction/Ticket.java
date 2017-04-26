9194167cc59d160a5addd7bf9e39b94f5bddc9ce
596854827 <596854827@qq.com>
2017-04-26 00:36:10 -0400

    Updata

--- BoffoRegister/src/transaction/Ticket.java
+++ BoffoRegister/src/transaction/Ticket.java
@@ -1,45 +1,41 @@
 package transaction;
 
+import database.BoffoDbObject;
 import events.BoffoEvent;
 import events.BoffoListenerInterface;
-import java.util.ArrayList;
 import java.util.List;
 import product.ProductObject;
 
-public class Ticket extends Transaction{
-    
+public class Ticket extends BoffoDbObject implements BoffoListenerInterface {
+
     private List<ProductObject> products;
-    
-    Ticket(List<ProductObject> products) {
-        
-    }
-    
+
     public Ticket() {
-        
+
     }
-    
+
     public ProductObject addProductbyUPC(String UPC) {
         ProductObject product = (ProductObject) ProductObject.loadByUpc(UPC);
         this.products.add(product);
         return product;
     }
-    
+
     public ProductObject addProductbyName(String name) {
         ProductObject product = (ProductObject) ProductObject.loadByName(name);
         this.products.add(product);
         return product;
     }
-    
+
     public void removeProductbyUPC(String UPC) {
         ProductObject product = (ProductObject) ProductObject.loadByUpc(UPC);
         this.products.remove(product);
     }
-    
+
     public void removeProductbyName(String name) {
         ProductObject product = (ProductObject) ProductObject.loadByName(name);
         this.products.remove(product);
     }
-    
+
     public double getTotalPrice(String String_price) {
         double totalPrice = 0;
         for (int i = 0; i < products.size(); i++) {
@@ -49,16 +45,16 @@ public class Ticket extends Transaction{
         }
         return totalPrice;
     }
-    
+
     public void fireEvent(BoffoEvent _event) {
     }
-    
+
     public void addListener(BoffoListenerInterface _event) {
     }
-    
+
     public void removeListener(BoffoListenerInterface _event) {
     }
-    
+
     public void messageReceived(BoffoEvent event) {
 
     }

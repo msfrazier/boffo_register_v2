481e165ae70117a6869c3fbb5a0ffbdcfc306fc3
Thomas Pedraza <Ltpedraz@uncg.edu>
2017-04-29 15:36:37 -0400

    Added new features.
    Authorization is now more versatile, instead of a minimum auth level,
    and checking auth level with a greater than or equal to expression;
    Other classes can add specified authorization groups to an arraylist
    that's inside the hash table. The isAuthorized method will check to see
    if current user is in a group that's permitted to access that action.
    Check authTest for more details and examples.

--- BoffoRegister/src/authorization/AuthTest.java
+++ BoffoRegister/src/authorization/AuthTest.java
@@ -4,10 +4,19 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+// This is a test and example class for the ones implementing it.
+
 public class AuthTest implements AuthorizationInterface {
-    protected static HashMap<String, ArrayList<Integer>> test = new HashMap<>();
+    protected static HashMap<String, ArrayList<Integer>> authTable = new HashMap<>();
 
     public static void main(String[] args) {
+        buildMap();
+        testAuthorization();
+    }
+    //building the HashMap
+    //EX: If managers and employees are the only ones that can do something (Not admin's or guests) , add
+    //1, 2 to the arraylist.
+        public static void buildMap () {
         ArrayList<Integer> addRemoveItemsToDB = new ArrayList<>();
         addRemoveItemsToDB.addAll(Arrays.asList(2, 3));
         ArrayList<Integer> checkoutCustomer = new ArrayList<>();
@@ -23,29 +32,29 @@ public class AuthTest implements AuthorizationInterface {
         ArrayList<Integer> viewPriceOfItem = new ArrayList<>();
         viewPriceOfItem.addAll(Arrays.asList(0, 1, 2, 3));
 
-        test.put("addRemoveItemsToDB", addRemoveItemsToDB);
-        test.put("checkoutCustomer", checkoutCustomer);
-        test.put("setStoreActions", setStoreActions);
-        test.put("addRemoveTicketItem", addRemoveTicketItem);
-        test.put("changeSystemSettings", changeSystemSettings);
-        test.put("viewPriceOfItem", viewPriceOfItem);
-        test.put("viewEmployeeSSN", viewEmployeeSSN);
-        if(Authorization.isAuthorized(addRemoveItemsToDB)){
-            System.out.println("Authorized!");
+        authTable.put("addRemoveItemsToDB", addRemoveItemsToDB);
+        authTable.put("checkoutCustomer", checkoutCustomer);
+        authTable.put("setStoreActions", setStoreActions);
+        authTable.put("addRemoveTicketItem", addRemoveTicketItem);
+        authTable.put("changeSystemSettings", changeSystemSettings);
+        authTable.put("viewPriceOfItem", viewPriceOfItem);
+        authTable.put("viewEmployeeSSN", viewEmployeeSSN);
         }
-        if(Authorization.isAuthorized(test.get("addRemoveItemsToDB"))){
+
+        public static void testAuthorization () {
+                    if(Authorization.isAuthorized(authTable.get("addRemoveItemsToDB"))){
             System.out.println("Authorized!");
         }
         else {
             System.out.println("Unauthorized!");
         }
-        if(Authorization.isAuthorized(test.get("viewPriceOfItem"))){
+        if(Authorization.isAuthorized(authTable.get("viewPriceOfItem"))){
             System.out.println("Authorized!");
         }
         else{
             System.out.println("Unauthorized!");
         }
-        if(Authorization.isAuthorized(test.get("changeSystemSettings"))){
+        if(Authorization.isAuthorized(authTable.get("changeSystemSettings"))){
             System.out.println("Authorized!");
         }
         else{
--- BoffoRegister/src/authorization/AuthorizationInterface.java
+++ BoffoRegister/src/authorization/AuthorizationInterface.java
@@ -1,6 +1,7 @@
 package authorization;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 
 /*
  *
@@ -9,14 +10,14 @@ import java.util.HashMap;
  *
  *  This Hashmap should be implemented as protected within Inventory,
     Administration, and Transaction's classes.
- *  Add all of the actions to plan to check into the hashmap as String, and
+ *  Add all of the actions one plans to check into the hashmap as String, and
     the corresponding authorization level needed as the int.
  *  Call the authorization class when needed to check if the
     current user has privilege to do an action.
  *  The authorization class "isAuthorized" will return a boolean based on
-    the current user and the minimum auth level.
+    the current user and the groups allowed in your ArrayList..
 */
 public interface AuthorizationInterface
 {
-    static HashMap<String, int[]> authTable = new HashMap<>();
+    static HashMap<String, ArrayList<Integer>> authTable = new HashMap<>();
 }

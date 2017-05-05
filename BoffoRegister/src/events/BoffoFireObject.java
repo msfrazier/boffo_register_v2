5a971af39beb26a2786ff850612f86b4e614ee57
msfrazier <msfrazie@uncg.edu>
2017-05-04 21:28:07 -0400

    Fixed things
    Fixed the Style, added a checkListener method and send to specific
    listener method

--- BoffoRegister/src/events/BoffoFireObject.java
+++ BoffoRegister/src/events/BoffoFireObject.java
@@ -12,35 +12,51 @@ to fire events
 import database.BoffoDbObject;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.IOException;
 
 public class BoffoFireObject extends BoffoDbObject {
-    //Lists the liseners an event firing class will have
+    //Lists the liseners an event firing class will have.
     private final List listeners = new ArrayList();
 
     public BoffoFireObject() {
     }
-    //Generic add and remove
-    public synchronized void addListener(BoffoListenerInterface listener) {
-        if (!this.listeners.contains(listener))
-            listeners.add(listener);
+    //Generic add and remove.
+    public synchronized void addListener(BoffoListenerInterface _listener) {
+        if (!this.listeners.contains(_listener))
+            listeners.add(_listener);
     }
 
-    public synchronized void removeListener(BoffoListenerInterface listener) {
-        if (this.listeners.contains(listener))
-            listeners.remove(listener);
+    public synchronized void removeListener(BoffoListenerInterface _listener) {
+        if (this.listeners.contains(_listener))
+            listeners.remove(_listener);
     }
 
     public synchronized void removeAllListeners() {
         listeners.clear();
     }
 
-    protected synchronized void fireEvent(BoffoEvent event) {
+    public synchronized boolean checkListener(BoffoListenerInterface _listener){
+        if(this.listeners.contains(_listener)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    protected synchronized void fireEvent(BoffoEvent _event) {
         // Clone the active listeners.
         Object[] tempList = this.listeners.toArray();
-        //Cycle through listeners and fire events
+        //Cycle through listeners and fire events.
         for (Object tempList1 : tempList) {
             BoffoListenerInterface tempObj = (BoffoListenerInterface) tempList1;
-            tempObj.messageReceived(event);
+            tempObj.messageReceived(_event);
+        }
+    }
+    //This method fires an event to only one specific listener.
+    protected synchronized void fireSpecificEvent(BoffoEvent _event, BoffoListenerInterface _listener){
+        if(this.checkListener(_listener)){
+            _listener.messageReceived(_event);
         }
     }
 }

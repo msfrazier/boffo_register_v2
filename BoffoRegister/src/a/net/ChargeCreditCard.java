4acb659ef6b4627da3ff0e23c6544e0982c3c7ce
cdhansen96 <cdhanse2@uncg.edu>
2017-05-04 21:53:47 -0400

    Minor Changes to ChargeCreditCard

--- BoffoRegister/src/a/net/ChargeCreditCard.java
+++ BoffoRegister/src/a/net/ChargeCreditCard.java
@@ -3,25 +3,18 @@ package a.net;
 import java.util.Random;
 import administration.Administration;
 
-class Charge{
-    public static void main(String[] args) {
-        Payment New  = BoffoPayment.processPayment(100.57,"4444444444444444", "AX3T5");
-        System.out.println(New.chargeResponse + " :: " + New.confirmation);
-    }
-}
-
 class BoffoPayment {
 
     public static Payment processPayment(double amount, String creditCardNumber, String ticketID) {
         // Here we would call on the Authorize.NET API, but instead, we'll use a stub.
         Payment pay = new Payment(creditCardNumber, amount);
-        Payment payResp = FakePayment.receivePayment(amount, ticketID, pay);
+        Payment payResp = TakePayment.receivePayment(amount, ticketID, pay);
         
         return payResp;
     }
 }
 
-class FakePayment {
+class TakePayment {
 
     public static Payment receivePayment(double amount, String ticketID, Payment p) {
         // Return a pretend confirmation number as though from Authorize.NET.
@@ -35,15 +28,15 @@ class FakePayment {
         switch (randomResponse) {
             case 1:
                 p.chargeRes("Payment Accepted");
-                p.confermation("Confirmation Number:" + randomConfirmation);
+                p.confermationNum(randomConfirmation);
                 break;
             case 2:
                 p.chargeRes("Insufficant Funds");
-                p.confermation("Confirmation Number:" + randomConfirmation);
+                p.confermationNum(randomConfirmation);
                 break;
             case 3:
                 p.chargeRes("Invalid Card Number");
-                p.confermation("Confirmation Number:" + randomConfirmation);
+                p.confermationNum(randomConfirmation);
                 break;
         }
 
@@ -51,18 +44,17 @@ class FakePayment {
     }
 }
 
-class Payment extends administration.Administration{
-//    final String secretKey = printSecretKey();
-//    final String transactionKey = printTransactionKey();
-//    final String currency = printCurrency();
+class Payment{
     String cardNumber;
     double chargeAmt;
+    int confirmationNum;
     String confirmation;
     String chargeResponse;
 
     public Payment(String cardNumber, double amt) {
         this.cardNumber = cardNumber;
         this.chargeAmt = amt;
+        this.confirmationNum = -1;
         this.chargeResponse = "No Card Charged";
         this.confirmation = "No Card Charged";
     }
@@ -71,8 +63,12 @@ class Payment extends administration.Administration{
         this.chargeAmt = C;
     }
 
-    public void confermation(String confNum) {
-        this.confirmation = confNum;
+    public void confermation(String conf) {
+        this.confirmation = conf;
+    }
+    
+    public void confermationNum(int confNum) {
+        this.confirmationNum = confNum;
     }
 
     public void chargeRes(String response) {
